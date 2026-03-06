3d4e5e728b98177635fcc57295210f3d49e3c082
lasval <52224175+lasval@users.noreply.github.com>
2026-03-06 16:48:42 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(string s) {
+    bool answer = true;
+    
+    if (s.length() != 4 && s.length() != 6)
+    {
+        answer = false;
+        return answer;
+    }
+    
+    for (int i = 0; i < s.length(); i++)
+    {
+        if ((int)s[i] >= '0' && (int)s[i] <= '9')
+        {
+            answer = true;            
+        }
+        else 
+        {
+            answer = false;
+            return answer;
+        }
+            
+    }
+    
+    
+    return answer;
+}
